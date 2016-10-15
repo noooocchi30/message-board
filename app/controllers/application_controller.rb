b0a4b563cf3eef2e436458a5e29f825156b4e5a5
keisuke <noooocchi30@gmail.com>
2016-10-15 06:36:45 +0000

    merge challenge1 to master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  
+   def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      # メッセージが保存できなかった時
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
+  end
+  
+  
+  
   protect_from_forgery with: :exception
 end
